0d700be9c9120f443e3f4633c1163a12015b68cc
Phil <birkal@outlook.com>
2015-07-25 15:02:25 -0700

    Add validate function

--- hw1.hs
+++ hw1.hs
@@ -9,9 +9,12 @@ toDigitsRev n =
 toDigits :: Integer -> [Integer]
 toDigits = reverse . toDigitsRev
 
+doubleEveryOtherLeft :: [Integer] -> [Integer]
+doubleEveryOtherLeft (x:y:xs) = x : y * 2 : doubleEveryOtherLeft xs
+doubleEveryOtherLeft a = a
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther (x:y:xs) = x : y * 2 : doubleEveryOther xs
-doubleEveryOther a = a
+doubleEveryOther = reverse . doubleEveryOtherLeft . reverse
 
 sumDigits :: [Integer] -> Integer
 sumDigits [] = 0
@@ -20,3 +23,6 @@ sumDigits (x:xs) =
     where
       firstDigit = if div x 10 == 0 then 0 else div x 10 
       lastDigit = if mod x 10 == x then x else mod x 10
+
+validate :: Integer -> Bool
+validate = (== 0) . (\x -> mod x 10) . sumDigits . doubleEveryOther . toDigits
