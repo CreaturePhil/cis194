e355e8dc6ff9583ec49b38920b7aad3309c4f69a
Phil <birkal@outlook.com>
2015-07-25 14:23:21 -0700

    Add toDigits and doubleEveryOther

--- hw1.hs
+++ hw1.hs
@@ -8,3 +8,15 @@ toDigitsRev n =
 
 toDigits :: Integer -> [Integer]
 toDigits = reverse . toDigitsRev
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther (x:y:xs) = x : y * 2 : doubleEveryOther xs
+doubleEveryOther a = a
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) =
+    firstDigit + lastDigit + sumDigits xs
+    where
+      firstDigit = if div x 10 == 0 then 0 else div x 10 
+      lastDigit = if mod x 10 == x then x else mod x 10
