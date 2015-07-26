8d14918177a2f0f431701a587b1128f9010fbc3b
Phil <birkal@outlook.com>
2015-07-25 18:01:18 -0700

    Add hanoi

--- hw1.hs
+++ hw1.hs
@@ -26,3 +26,9 @@ sumDigits (x:xs) =
 
 validate :: Integer -> Bool
 validate = (== 0) . (\x -> mod x 10) . sumDigits . doubleEveryOther . toDigits
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
