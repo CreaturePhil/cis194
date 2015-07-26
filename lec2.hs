21eb2f4d404ec30bf438bd66585c9536a224e295
Phil <birkal@outlook.com>
2015-07-26 16:03:22 -0700

    Add lecture 2

---
+++ lec2.hs
@@ -0,0 +1,48 @@
+data Thing = Shoe
+           | Ship
+           | SealingWax
+           | Cabbage
+           | King
+  deriving Show
+
+shoe :: Thing
+shoe = Shoe
+
+listO'Things :: [Thing]
+listO'Things = [Shoe, SealingWax, King, Cabbage, King]
+
+isSmall :: Thing -> Bool
+isSmall Shoe = True
+isSmall Ship = False
+isSmall SealingWax = True
+isSmall Cabbage = True
+isSmall King = False
+
+isSmall2 :: Thing -> Bool
+isSmall2 Ship = False
+isSmall2 King = False
+isSmall2 _    = True
+
+data FailableDouble = Failure
+                    | OK Double
+                    deriving Show
+
+safeDiv :: Double -> Double -> FailableDouble
+safeDiv _ 0 = Failure
+safeDiv x y = OK (x / y)
+
+failureToZero :: FailableDouble -> Double
+failureToZero Failure = 0
+failureToZero (OK d)  = d
+
+data Person = Person String Int Thing
+    deriving Show
+
+phil :: Person
+phil = Person "Phil" 18 King
+
+getAge :: Person -> Int
+getAge (Person _ a _) = a
+
+baz :: Person -> String
+baz p@(Person n _ _) = "The name field of (" ++ show p ++ ") is " ++ n
