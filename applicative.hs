baf4b19d864cd71a7b1d7e65a36fea40f6b13e02
Phil <birkal@outlook.com>
2015-08-16 16:35:03 -0700

    Applicative Functor

---
+++ applicative.hs
@@ -0,0 +1,8 @@
+class (Functor f) => Applicative f where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
+
+instance Applicative Maybe where
+    pure = Just
+    Nothing <*> _ = Nothing
+    (Just f) <*> something = fmap f something
